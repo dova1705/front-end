923e875a3a570e7157b89ef90a53353d76a03d9d
dova1 <allakov.dovlet@gmail.com>
2024-01-31 20:49:53 +0300

    hw_8 js array

--- lesson_8/hw_8/script.js
+++ lesson_8/hw_8/script.js
@@ -6,3 +6,15 @@ for(let i = 0; i <names.length; i++){
     namesAges[i] = `${names[i]} ${ages[i]} лет/годов`
 }
 console.log(namesAges);
+
+const name = "Семен";
+const age = 33;
+const text = `Меня зовут ${name} и мне ${age} года. Я изучаю язык JavaScript`;
+console.log(text);
+
+const reversed = [];
+for(let i = 0; i < namesAges.length; i++){
+    reversed.unshift(namesAges[i])
+}
+console.log(reversed); //массив в обратном порядке
+console.log(namesAges); // исходный массив остался без изменений
